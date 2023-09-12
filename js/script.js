ed85ab934d046911839cd8588d47c16531adb7d9
juaanlopez <juaannpablo@outlook.com>
2023-09-12 00:14:10 -0300

    mantener modo oscuro si el usuario deseamejoraenjs

--- js/script.js
+++ js/script.js
@@ -1,42 +1,16 @@
 botonDark = document.querySelector("#botonDark")
-// barranav = document.querySelector("nav")
-// caracteristicas = document.querySelector(".caracteristicas")
-
-// function switchDark(){
-//     barranav.classList.toggle("nav-dark")
-//     botonDark.classList.toggle("btn-light")
-//     document.querySelector(".titulocarrusel").style.color = "#f9d806"
-//     document.querySelector("#titulocategoria").style.color = "#f9d806"
-//     document.querySelector("#tituloofertas").style.color = "#f9d806"
-//     document.querySelector("#titulopreciosjustos").style.color = "#f9d806"
-//     caracteristicas.classList.toggle("carac-dark")
-//     document.querySelector("#cafe").style.color = "#f9d806"
-//     document.querySelector("#preciocafe").style.color = "#f9d806"
-//     document.querySelector("#chocolatada").style.color = "#f9d806"
-//     document.querySelector("#chocolatadaprecio").style.color = "#f9d806"
-//     document.querySelector("#queso").style.color = "#f9d806"
-//     document.querySelector("#quesoprecio").style.color = "#f9d806"
-//     document.querySelector("#milanesa").style.color = "#f9d806"
-//     document.querySelector("#milanesaprecio").style.color = "#f9d806"
-//     document.querySelector("#manteca").style.color = "#f9d806"
-//     document.querySelector("#mantecaprecio").style.color = "#f9d806"
-//     document.querySelector("#jugo").style.color = "#f9d806"
-//     document.querySelector("#jugoprecio").style.color = "#f9d806"
-//     document.querySelector("#aceite").style.color = "#f9d806"
-//     document.querySelector("#aceiteprecio").style.color = "#f9d806"
-//     document.querySelector("#citrus").style.color = "#f9d806"
-//     document.querySelector("#citrusprecio").style.color = "#f9d806"
-//     document.querySelector("#alfajor").style.color = "#f9d806"
-//     document.querySelector("#alfajorprecio").style.color = "#f9d806"
-//     document.querySelector("#arroz").style.color = "#f9d806"
-//     document.querySelector("#arrozprecio").style.color = "#f9d806"
-//     document.querySelector("#azucar").style.color = "#f9d806"
-//     document.querySelector("#azucarprecio").style.color = "#f9d806"
-//     document.querySelector("#galletitas").style.color = "#f9d806"
-//     document.querySelector("#galletitasprecio").style.color = "#f9d806"
-//     document.body.classList.toggle("fondo-dark")
-// }
+let body = document.body
 
 botonDark.addEventListener("click", () => {
-    document.body.classList.toggle("modo_oscuro")
+    botonDark.classList.toggle("modonoche")
+    let val=body.classList.toggle("modo_oscuro")
+    localStorage.setItem("botonDark",val)
 })
+
+let valor=localStorage.getItem("botonDark")
+
+if (valor=="true") {
+    body.classList.add("modo_oscuro")
+} else {
+    body.classList.remove("modo_oscuro")
+}
