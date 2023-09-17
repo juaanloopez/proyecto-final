3fd5ccc3ac64a74c51acb6a403fa5586f027fa0d
juaanlopez <juaannpablo@outlook.com>
2023-09-16 22:07:38 -0300

    barra de busqueda con error solocaptaun h3ynotodos

--- js/script.js
+++ js/script.js
@@ -22,3 +22,42 @@ if (valor == "true"){
     body.classList.remove("modo_oscuro")
     botonDark.classList.remove("modonoche")
 }
+
+// constantes
+
+const lis = document.querySelectorAll(".ofertasdeldia-preciosjusto .contenedor-ofertasdeldia-preciosjusto .card-ofertasdeldia-preciosjusto h3")
+const buscador =document.querySelector("#buscador")
+
+// funciones
+
+for(i=0;i<lis.length;i+=2){
+    lis[i].classList.add("list-group-item-secondary")
+}
+
+function filtrar(substring, elements){
+    cumplen = []
+    for (i of elements){
+        if (i.innerText.toLowerCase().includes(substring.toLowerCase())){
+            cumplen.push(i)
+        }
+        return cumplen
+    }
+}
+
+// eventos
+
+buscador.addEventListener("input", () =>{
+    if(buscador.value == ""){
+        for (i of lis){
+            i.classList.remove("d-none")
+        }
+    }else{
+        filtrados = filtrar(buscador.value,lis)
+        for(i of lis){
+            i.classList.add("d-none")
+        }
+        for (i of filtrados){
+            i.classList.remove("d-none")
+        }
+    }
+})
